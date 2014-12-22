1dc4c02d34ceef4efcb126723d976d7e46be2b13
Jeroen Ooms <jeroenooms@gmail.com>
2014-12-21 16:35:32 -0800

    Add dynamic forms

--- js/app.js
+++ js/app.js
@@ -8,5 +8,32 @@ $(function(){
 	});
 
 	$(".panel-group").sortable({ handle: ".panel-heading" })
-	$(".list-group").sortable()
+
+	$("#new_survey_button").click(function(e){
+		e.preventDefault();
+		var el = $("#surveytemplate .survey_content").clone();
+		var id = el.children(".panel-collapse").uniqueId()[0].id;
+		el.find("h4.panel-title a").attr("href", "#" + id);
+		el.find(".list-group").sortable();
+		el.find(".dropdown-menu a").click(function(e){
+			e.preventDefault();
+			var prompt_type = $(this).data("type");
+			add_prompt(prompt_type, el)
+		});
+		el.find(".delete_survey_button").click(function(e){
+			e.preventDefault();
+			el.hide('slow', function(){
+				el.remove()
+			});
+		});
+		$("#surveygroup").append(el);
+		$("#surveygroup").find(".collapse").collapse('hide');
+		el.find(".collapse").collapse('show');
+	});
+
+	function add_prompt(prompt_type, el){
+		el.find(".prompt_list").append(function(){
+			return '<a class="list-group-item"> _blank <span class="badge">' + prompt_type + '</span></a>'
+		})
+	}
 });

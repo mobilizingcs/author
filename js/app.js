847cac8971f633ebf2eb06051e0e4ac636691921
Jeroen Ooms <jeroenooms@gmail.com>
2015-02-07 18:32:16 -0800

    add lots of stuff

--- js/app.js
+++ js/app.js
@@ -55,15 +55,17 @@ $(function(){
 		});
 
 		function add_prompt(prompt_type, el){
-			el.find(".prompt_list").append(function(){
+			el.find(".survey_prompt_list").append(function(){
 				var a = $(Mustache.render(templates.promptlink, {
 					prompt_type:prompt_type
-				}));
+				})).data("prompt_type", prompt_type);
+				var content = popover_content(prompt_type, a);
 				a.popover({
 					html: true,
+					//placement: "top",
 					trigger: "click",
 					title : prompt_type + " prompt",
-					content: popover_content(prompt_type, a)
+					content: content
 				}).click(function(e){
 					e.preventDefault();
 				}).on("show.bs.popover", function(){
@@ -91,6 +93,7 @@ $(function(){
 			$.each(fields, function(index, fieldname){
 				var field = logic.fields[fieldname];
 				var output = Mustache.render(templates[field.type], {
+					field : fieldname,
 					label : field.label || toTitleCase(fieldname),
 					placeholder : field.placeholder || "Please enter " + fieldname.toLowerCase()
 				});
@@ -130,3 +133,44 @@ function toTitleCase(str){
 	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
 
+/* Convert Form to XML. Should rewrite this in proper MVC */
+function form2xml(){
+	var surveys = $("#surveygroup .survey_form");
+	surveys.each(function(){
+
+		/* get form */
+		var form = $(this);
+
+		/* survey fields */
+		var survey_id = form.find(".survey_id_field").val()
+		var survey_title = form.find(".survey_title_field").val()
+		var survey_description = form.find(".survey_description_field").val()
+		var survey_submit = form.find(".survey_submit_field").val()
+		var survey_anytime = form.find(".survey_anytime_field").val()
+
+		/* find prompts */
+		var survey_prompt_list = form.find(".survey_prompt_list .prompt_link")
+		survey_prompt_list.each(function(){
+			var prompt_link = $(this);
+			var prompt_type = prompt_link.data("prompt_type")
+			var popover = prompt_link.data("bs.popover").$tip;
+			var fields = $(popover).find(".prompt_field")
+			console.log("Prompt: " + prompt_type)
+			fields.each(function(){
+				var field = $(this);
+				var name = field.data("field")
+				var value = getFieldValue(field)
+				console.log(name + " = " + value)
+			})
+		})
+	});
+}
+
+function getFieldValue(el){
+	if(el.is("input[type=checkbox]")){
+		return el.is(':checked')
+	} else if(el.is("ol")){
+		return "Not implemented yet..."
+	}
+	return el.val();
+}
--- js/templates.js
+++ js/templates.js
@@ -1,10 +1,11 @@
 (function(global){
 	global.templates = {
-		"urn" : '<div class="form-group"><label>{{label}}</label><input class="form-control" placeholder="{{placeholder}}"></div>',
-		"string" : '<div class="form-group"><label>{{label}}</label><input class="form-control" placeholder="{{placeholder}}"></div>',
-		"bool" : '<div class="checkbox"><label><input type="checkbox">{{label}}</label></div>',
-		"number" : '<div class="form-group"><label>{{label}}</label><input type="number" class="form-control" placeholder="{{placeholder}}"></div>',
-		"keyval" : '<div class="form-group"><label>{{label}}</label><ol class="choice_values"></ol></div>',
+		"urn" : '<div class="form-group"><label>{{label}}</label><input class="form-control prompt_field" data-field="{{field}}" placeholder="{{placeholder}}"></div>',
+		"string" : '<div class="form-group"><label>{{label}}</label><input class="form-control prompt_field" data-field="{{field}}" placeholder="{{placeholder}}"></div>',
+		"text" : '<div class="form-group"><label>{{label}}</label><textarea rows="2" class="form-control prompt_field" data-field="{{field}}" placeholder="{{placeholder}}"></textarea></div>',
+		"bool" : '<div class="checkbox"><label><input type="checkbox" class="prompt_field" data-field="{{field}}">{{label}}</label></div>',
+		"number" : '<div class="form-group"><label>{{label}}</label><input type="number" class="form-control prompt_field" data-field="{{field}}" placeholder="{{placeholder}}"></div>',
+		"keyval" : '<div class="form-group"><label>{{label}}</label><ol class="choice_values prompt_field" data-field="{{field}}"></ol></div>',
 		"promptlink" : '<a role="button" class="list-group-item prompt_link" href="#"> <span class="prompt_id_text">_blank</span> <span class="badge">{{prompt_type}}</span></a>'
 	}
 })(window);

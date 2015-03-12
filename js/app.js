938db4e5d46f1c7ce224923439a4b03d6e7a8861
Jeroen Ooms <jeroenooms@gmail.com>
2015-03-11 19:35:14 -0700

    updates

--- js/app.js
+++ js/app.js
@@ -2,6 +2,37 @@ $.getJSON("logic.json", function(logic){
 
 	//globals
 	var oldpop;
+	var oh = Ohmage("/app", "surveytool")
+
+	//attach global callbacks
+	oh.callback("done", function(x, status, req){
+		//for debugging only
+		//console.log(x);
+	})
+
+	//global error handler. In ohmage 200 means unauthenticated
+	oh.callback("error", function(msg, code, req){
+		(code == 200) ? window.location.replace("/web/#login") : alert("Error!\n" + msg);
+	});
+
+	//initiate ohmage client
+	oh.user.whoami().done(function(username){
+
+		//make sure we don't timeout
+		oh.keepalive();
+
+		//get available classes
+		oh.user.info().done(function(x){
+			var classlist = {};
+			$.each(x[username].classes, function(urn, name){
+				classlist[name] = urn;
+			});
+			$.each(Object.keys(classlist).sort(), function(i, name){
+				var urn = classlist[name];
+				$("#class_urn_field").append($("<option/>").val(urn).text(name));
+			})
+		})
+	});
 
 	//XML parser for mixed case tag names (HTML only supports lowercase tags)
 	var parse = (function(parser, jQuery){
@@ -409,6 +440,25 @@ $.getJSON("logic.json", function(logic){
 		}
 		filereader.readAsText(this.files[0]);
 	})
+
+	//upload button
+	$("#create_campaign_button").click(function(e){
+		e.preventDefault();
+		var campaign_name = $("#campaign_name_field").val() || alert("Invalid campaign name");
+		var campaign_urn = $("#campaign_urn_field").val() || alert("Invalid campaign urn");
+		var class_urn = $("#class_urn_field").val() || alert("Invalid class");
+
+		oh.campaign.create({
+			running_state : "stopped",
+			privacy_state : "private",
+			campaign_urn : campaign_urn,
+			campaign_name : campaign_name,
+			class_urn_list : class_urn,
+			xml : writexml()
+		}).done(function(){
+			alert("success!")
+		});
+	});
 
 	//init page
 	writexml();

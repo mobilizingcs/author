e80e361be99024c64e20cac80b70d8bf28544d39
Jeroen Ooms <jeroenooms@gmail.com>
2015-07-16 16:34:39 +0200

    update login link

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ $.getJSON("logic.json", function(logic){
 
 	//global error handler. In ohmage 200 means unauthenticated
 	oh.callback("error", function(msg, code, req){
-		(code == 200) ? window.location.replace("../web/#login") : alert("Error!\n" + msg);
+		(code == 200) ? window.location.replace("/#login") : alert("Error!\n" + msg);
 	});
 
 	//initiate ohmage client
